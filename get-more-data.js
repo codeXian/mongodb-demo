c85286337b3068fad01046ed000c7c681598ce94
zhengxianfeng <codexian@yeah.net>
2018-02-02 15:57:36 +0800

    😅 生成200W随机数据量

---
+++ get-more-data.js
@@ -0,0 +1,46 @@
+// 生成随机数
+
+function GetRandomNum(min, max) {
+  let range = max - min;
+  let rand = Math.random();
+  return (min + Math.round(rand * range));
+}
+
+// console.log(GetRandomNum(10000, 99999));
+
+// 生成随机用户名
+
+function GetRandomUserName(min, max) {
+  let tempStringArray = "123456789qwertyuiopasdfghjklzxcvbnm".split("");
+  let outPuttext = "";
+
+  for (let i = 1; i < GetRandomNum(min, max); i++) {
+    outPuttext = outPuttext + tempStringArray[GetRandomNum(0, tempStringArray.length)]
+  }
+
+  return outPuttext;
+}
+
+// console.log(GetRandomUserName(7, 16));
+
+var db = connect("company");
+db.randomInfo.drop();
+var tempInfo = [];
+for (let i = 0; i < 2000000; i++) {
+  tempInfo.push({
+    username: GetRandomUserName(7, 16),
+    regeditTime: new Date(),
+    randNum0: GetRandomNum(100000, 999999),
+    randNum1: GetRandomNum(100000, 999999),
+    randNum2: GetRandomNum(100000, 999999),
+    randNum3: GetRandomNum(100000, 999999),
+    randNum4: GetRandomNum(100000, 999999),
+    randNum5: GetRandomNum(100000, 999999),
+    randNum6: GetRandomNum(100000, 999999),
+    randNum7: GetRandomNum(100000, 999999),
+    randNum8: GetRandomNum(100000, 999999),
+    randNum9: GetRandomNum(100000, 999999)
+  })
+}
+
+db.randomInfo.insert(tempInfo);
